d1a46a0f680fe07226241fe10b10aea7c37cd358
Nelson Lopes <nelson.npl@gmail.com>
2019-02-28 11:32:18 -0400

    commit

---
+++ csharp/Hackerrank/Program01.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+/*
+ * Hackerrank
+ * Animal Inheritance
+ * Date: 2018.02.28
+ * Author: Nelson Lopes
+*/
+namespace Animal
+{
+	public abstract class Animal
+	{
+		protected bool isMammal;
+		protected bool isCarnivorous;
+
+		public Animal(bool isMammal, bool isCarnivorous)
+		{
+			this.isMammal = isMammal;
+			this.isCarnivorous = isCarnivorous;
+		}
+
+		public bool getIsMammal()
+		{
+			return this.isMammal;
+		}
+
+		public bool getIsCarnivorous()
+		{
+			return this.isCarnivorous;
+		}
+
+		abstract public string getGreeting();
+
+		public void printAnimal(string name)
+		{
+			Console.WriteLine("A {0} says '{1}', is{2} carnivorous, and is{3} a mammal.",
+			name,
+			this.getGreeting(),
+			this.getIsCarnivorous() ? "" : " not",
+			this.getIsMammal() ? "" : " not");
+		}
+	}
+
+	// Write your code here.
+
+	/**
+    *   Dog class
+    **/
+	public class Dog : Animal
+	{
+		public Dog() : base(true, true) { }
+
+		public Dog(bool isMammal, bool isCarnivorous) : base(isMammal, isCarnivorous)
+		{
+		}
+
+		public override string getGreeting()
+		{
+			return "ruff";
+		}
+	}
+
+	/**
+    *   Cow class
+    **/
+	public class Cow : Animal
+	{
+		public Cow() : base(true, false) { }
+
+		public override string getGreeting()
+		{
+			return "moo";
+		}
+	}
+
+	/**
+    *   Duck class
+    **/
+	public class Duck : Animal
+	{
+		public Duck() : base(false, false) { }
+
+		public override string getGreeting()
+		{
+			return "quack";
+		}
+	}
+
+	class Solution
+	{
+		//static void Main(string[] args)
+		//{
+		//	Animal dog = new Dog();
+		//	dog.printAnimal("dog");
+
+		//	Animal cow = new Cow();
+		//	cow.printAnimal("cow");
+
+		//	Animal duck = new Duck();
+		//	duck.printAnimal("duck");
+		//}
+	}
+}
---
+++ csharp/Hackerrank/Program02.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+ * Hackerrank
+ * Work Schedule
+ * Date: 2018.02.28
+ * Author: Nelson Lopes
+*/
+
+class Result
+{
+	public static List<string> findSchedules(int workHours, int dayHours, string pattern)
+	{
+
+		List<List<int>> list = new List<List<int>>();
+
+		// 1 <= work_hours <= 56
+		if (0 > workHours || workHours > 56)
+			return new List<string>();
+
+		// 1 <= day_hours <= 8
+		if (0 > dayHours || dayHours > 8)
+			return new List<string>();
+
+		// | pattern | = 7
+		if (pattern.Length != 7)
+			return new List<string>();
+
+		// Each character of pattern E {0,1,...,8}
+		if (pattern.Except("012345678?".ToList()).Any())
+			return new List<string>();
+
+		BuildWeekList(dayHours, workHours, 0, pattern, null, ref list);
+
+		return list.Select(x => string.Join(string.Empty, x)).ToList();
+	}
+
+	public static void BuildWeekList(int dh, int wh, int pi, string p, List<int> w, ref List<List<int>> l)
+	{
+		if (w == null)
+			w = new List<int>();
+
+		var i = pi + 1;
+
+		if (p[pi] == '?')
+			for (int h = 0; h <= dh; h++)
+			{
+				if (AddHourDay(dh, wh, i, h, p, ref w, ref l))
+					break;
+			}
+		else
+			AddHourDay(dh, wh, i, Convert.ToInt32(p[pi].ToString()), p, ref w, ref l);
+	}
+
+	private static bool AddHourDay(int dh, int wh, int pi, int h, string p, ref List<int> w, ref List<List<int>> l)
+	{
+		var myWeek = new List<int>(w) { h };
+		if (pi == p.Length)
+		{
+			if (myWeek.Sum() == wh)
+			{
+				l.Add(myWeek);
+				return true;
+			}
+		}
+		else
+			BuildWeekList(dh, wh, pi, p, myWeek, ref l);
+
+		return false;
+	}
+}
+
+class Solution
+{
+	public static void Main(string[] args)
+	{
+
+		int workHours = Convert.ToInt32(Console.ReadLine().Trim());
+
+		int dayHours = Convert.ToInt32(Console.ReadLine().Trim());
+
+		string pattern = Console.ReadLine();
+
+		List<string> result = Result.findSchedules(workHours, dayHours, pattern);
+
+		Console.WriteLine((String.Join("\n", result)));
+
+		Console.ReadLine();
+	}
+}
